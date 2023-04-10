118aca19055b9683ab1d824ed05434990bf52928
s.aran <s-aran@users.noreply.github.com>
2023-04-10 23:41:44 +0900

    ported to rust

---
+++ zenhan-rs/src/main.rs
@@ -0,0 +1,52 @@
+use std::env;
+use windows::Win32::{
+    Foundation::{HWND, LPARAM, LRESULT, WPARAM},
+    UI::{
+        Input::Ime::ImmGetDefaultIMEWnd,
+        WindowsAndMessaging::{GetForegroundWindow, SendMessageW, WM_IME_CONTROL},
+    },
+};
+
+static IMC_GETOPENSTATUS: WPARAM = WPARAM(5);
+static IMC_SETOPENSTATUS: WPARAM = WPARAM(6);
+
+fn main() {
+    let argv: Vec<String> = env::args().collect();
+    let argc = argv.len();
+
+    let h_wnd: HWND = unsafe { GetForegroundWindow() };
+    if h_wnd == HWND(0) {
+        return ();
+    }
+
+    let h_ime: HWND = unsafe { ImmGetDefaultIMEWnd(h_wnd) };
+    if h_ime == HWND(0) {
+        return ();
+    }
+
+    let stat: LRESULT;
+    if argc < 2 {
+        stat = unsafe { SendMessageW(h_ime, WM_IME_CONTROL, IMC_GETOPENSTATUS, LPARAM(0)) };
+    } else {
+        let mode: i32 = match argv.get(1).unwrap_or(&"0".to_string()).parse() {
+            Ok(i) => i,
+            Err(e) => {
+                eprintln!("{}", e.to_string(),);
+                return ();
+            }
+        };
+        unsafe {
+            SendMessageW(
+                h_ime,
+                WM_IME_CONTROL,
+                IMC_SETOPENSTATUS,
+                LPARAM(mode as isize),
+            )
+        };
+        stat = LRESULT(mode as isize);
+    }
+
+    println!("{}", stat.0);
+
+    ()
+}
